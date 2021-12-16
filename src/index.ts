e478299c6bedb5144f469434d4812d85a4afe235
PrinceNarteh <prinarteh@gmail.com>
2021-12-16 13:10:54 +0000

    adds express server

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { User } from "./entity/User";
+import * as express from "express";
+const PORT = process.env.PORT || 4000;
 
 createConnection()
   .then(async (connection) => {
-    console.log("Database connected successfully!");
+    const app = express();
+
+    app.get("/", (req, res) => {
+      res.send("Hello, World!");
+    });
+
+    app.listen(PORT, () =>
+      console.log(`Server running on http://localhost:${PORT}/graphql`)
+    );
   })
   .catch((error) => console.log(error));
