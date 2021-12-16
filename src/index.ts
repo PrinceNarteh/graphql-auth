4b7bb7ac19ed12b27fef58008598a2ce71582ed4
PrinceNarteh <prinarteh@gmail.com>
2021-12-16 13:27:51 +0000

    adds apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,31 @@
 import "reflect-metadata";
+import { AuthResolver } from "./modules/user/auth.resolver";
+import { ApolloServer } from "apollo-server-express";
+import cors from "cors";
+import express from "express";
+import morgan from "morgan";
+import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
-import * as express from "express";
+
 const PORT = process.env.PORT || 4000;
 
 createConnection()
   .then(async (connection) => {
+    // instantiate express app
     const app = express();
+
+    // adding middleware
+    app.use(cors());
+    app.use(morgan("dev"));
+
+    // setup apollo server
+    const apolloServer = new ApolloServer({
+      schema: await buildSchema({
+        resolvers: [AuthResolver],
+      }),
+    });
+    await apolloServer.start();
+    apolloServer.applyMiddleware({ app });
 
     app.get("/", (req, res) => {
       res.send("Hello, World!");
