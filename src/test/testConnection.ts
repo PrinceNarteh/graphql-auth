d79bc07dcbb51ab90757909f331b60e26ec21e67
PrinceNarteh <prinarteh@gmail.com>
2021-12-17 18:41:37 +0000

    setup jest testing environment

---
+++ src/test/testConnection.ts
@@ -0,0 +1,23 @@
+import { createConnection } from "typeorm";
+
+export const testConnection = (drop:boolean =false) => {
+  return createConnection({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: "postgres",
+    password: "admin",
+    database: "graphql_auth",
+    logging: false,
+    dropSchema: drop,
+    synchronize: drop,
+    entities: ["src/entity/**/*.ts"],
+    migrations: ["src/migration/**/*.ts"],
+    subscribers: ["src/subscriber/**/*.ts"],
+    cli: {
+      entitiesDir: "src/entity",
+      migrationsDir: "src/migration",
+      subscribersDir: "src/subscriber",
+    },
+  };)
+};
